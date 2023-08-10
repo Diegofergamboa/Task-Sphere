ea72e90115cfb15c1d0da01d8fc0954d3871010c
diegofergamboa <diegofergamboa@hotmail.es>
2023-08-10 17:58:58 -0500

    Changes on the React effect

--- src/App/useLocalStorage.js
+++ src/App/useLocalStorage.js
@@ -1,16 +1,22 @@
 import React, { useState } from 'react'
 
 const useLocalStorage = (itemName, initialValue) => {
-    const localStorageItem = localStorage.getItem(itemName)
+    
+const useLocalStorage = (itemName, initialValue) => {
+    const [item, setItem] = React.useState(initialValue)
+    const [loading, setLoading] = React.useState(false)
+    const [error, setError] = React.useState(false)
 
-    let parsedItem
-    if (!parsedItem) {
-        localStorage.setItem(itemName, JSON.stringify(initialValue))
-        parsedItem = initialValue
-    } else (
-        parsedItem = JSON.parse(localStorageItem)
-    )
-    const [item, setItem] = React.useState(parsedItem);
+    React.useEffect(() => {
+        const localStorageItem = localStorage.getItem(itemName)
+        let parsedItem
+        if (!parsedItem) {
+            localStorage.setItem(itemName, JSON.stringify(initialValue))
+            parsedItem = initialValue
+        } else (
+            parsedItem = JSON.parse(localStorageItem)
+        )
+    })
 
     const saveItem = (newItem) => {
         localStorage.setItem(itemName, JSON.stringify(newItem))
@@ -19,6 +25,5 @@ const useLocalStorage = (itemName, initialValue) => {
 
     return [item, saveItem]
 }
-
 
 export { useLocalStorage }
