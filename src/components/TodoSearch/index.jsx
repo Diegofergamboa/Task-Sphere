d1c44d7b099aa74668e0ca936746e880ea1187b1
diegofergamboa <diegofergamboa@hotmail.es>
2023-08-12 18:25:50 -0500

    Fixed react context with TodoSearch

--- src/components/TodoSearch/index.jsx
+++ src/components/TodoSearch/index.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { todoContext } from '../../TodoContext'
+import './styles.css'
 
-const TodoSearch = ({searchValue , setSearchValue}) => {
+const TodoSearch = () => {
+  const {
+    searchValue,
+    setSearchValue
+  } = React.useContext(todoContext)
   return (
-    <input className='TodoSearch' placeholder='Search your task' onChange={(event) => setSearchValue(event.target.value)} value={searchValue}/> 
+    <input
+      className='TodoSearch'
+      placeholder='Search your task'
+      onChange={(event) => setSearchValue(event.target.value)} value={searchValue}
+    />
   )
 }
 
